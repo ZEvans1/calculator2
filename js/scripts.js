dedfc7f350248215487baebdccd1fe35963bca57
Edgar hernandez and Zach Evans <zach@>
2017-11-30 14:40:57 -0800

    add scripts.js and function

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
